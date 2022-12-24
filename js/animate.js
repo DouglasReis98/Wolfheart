8a5533438dbc137ed00c4fd21d283e1aaf298d4d
DouglasReis <douglasamaralreis@outlook.com>
2022-12-23 21:48:04 -0300

    fix

--- js/animate.js
+++ js/animate.js
@@ -1,45 +1,18 @@
-// Lógica da animação:
-// 1 - Selecionar elementos que devem ser animados
-// 2 - Definir a classe que é adicionada durante a animação
-// 3 - Criar função de animação
-//  3.1 - Verificar a distância entre a barra de scroll e o topo do site
-//  3.2 - Verificar se a distância do 3.1 + Offset é maior do que a distância entre o elemento e o Topo da Página.
-//  3.3 - Se verdadeiro adicionar classe de animação, remover se for falso.
-// 4 - Ativar a função de animação toda vez que o usuário utilizar o Scroll
-// 5 - Otimizar ativação
+window.addEventListener('scroll', cards);
 
-// Debounce do Lodash
-/*const debounce = function(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-*/
-const target = document.querySelectorAll('[data-card]');
-const animationClass = 'animate';
+function cards() {
+    let revela = document.querySelectorAll('[data-card]');
+    const animaClass = 'animate';
 
-function animaScroll() {
-    const windowTop = window.pageYOffset - 250;
-    target.forEach(function(element) {
-      if ((windowTop) > element.offsetTop) {
-        element.classList.add(animationClass);
+    for (let i = 0; i < revela.length; i++) {
+      let windowHeight = window.innerHeight;
+      let revelaTop = revela[i].getBoundingClientRect().top;
+      let valorReferencia = 180;
+      
+      if (revelaTop < windowHeight - valorReferencia) {
+        revela[i].classList.add(animaClass);
+      } else{
+        revela[i].classList.remove(animaClass);
       }
-
-    })
+    }
 }
-
-animaScroll();
-
-
-window.addEventListener('scroll', function(){
-    animaScroll();
-})
